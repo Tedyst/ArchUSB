b5f95fb897cc3b3e9b61d0f68853de94595ff907
Stoica <stoicatedy@gmail.com>
2019-08-20 08:23:14 +0300

    added icon theme

--- airootfs/home/tedy/autostart.sh
+++ airootfs/home/tedy/autostart.sh
@@ -1,6 +1,7 @@
 xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitorVirtual-1/workspace0/last-image --set /home/tedy/bg.png
 xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/image-path --set /home/tedy/bg.png
 xfconf-query -c xsettings -p /Net/ThemeName -s "Adapta-Nokto"
+xfconf-query -c xsettings -p /Net/IconThemeName -s "Papirus Dark"
 xfconf-query -c xfwm4 -p /general/workspace_count -s 1
 
 cp /usr/share/applications/visual-studio-code.desktop /home/tedy/Desktop
