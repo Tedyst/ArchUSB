ee9f69fcf454a4b7232ba0fadbc32758db826b45
Stoica Tedy <stoicatedy@gmail.com>
2020-10-03 11:20:09 +0300

    asd

--- generateiso.sh
+++ generateiso.sh
@@ -36,58 +36,6 @@ createlsbrelease() {
   echo "DISTRIB_RELEASE=$distroversion" | sudo tee --append ./workingdir/airootfs/etc/lsb-release > /dev/null
   echo "DISTRIB_CODENAME=$distrocodename" | sudo tee --append ./workingdir/airootfs/etc/lsb-release > /dev/null
 }
-compilecalamares() {
-  echo "Preparing Repository..."
-  mkdir customrepo
-  mkdir customrepo/x86_64
-  mkdir customrepo/i686
-  mkdir customrepo/archmaker-calamares
-  echo "Preparing Calamares-build..."
-  curl http://archmaker.guidedlinux.org/PKGBUILD > customrepo/archmaker-calamares/PKGBUILD
-  FILES="calamaresslides/*"
-  currentslide=1
-  for f in $FILES
-  do
-  currentline=$(( $currentslide + 27 ))
-  sed -i "${currentline}a\ \ \ \ cp $(pwd)\/$f src\/branding\/custombranding\/" ./customrepo/archmaker-calamares/PKGBUILD
-  echo "    echo '    ' >> src/branding/custombranding/show.qml" >> slideshowchanges
-  echo "    echo '    Slide {' >> src/branding/custombranding/show.qml" >> slideshowchanges
-  echo "    echo '        ' >> src/branding/custombranding/show.qml" >> slideshowchanges
-  echo "    echo '        Image {' >> src/branding/custombranding/show.qml" >> slideshowchanges
-  echo "    echo '            id: background${currentslide}' >> src/branding/custombranding/show.qml" >> slideshowchanges
-  echo "    echo '            source: \"${f##*/}\"' >> src/branding/custombranding/show.qml" >> slideshowchanges
-  echo "    echo '            width: 800; height: 440' >> src/branding/custombranding/show.qml" >> slideshowchanges
-  echo "    echo '            fillMode: Image.PreserveAspectFit' >> src/branding/custombranding/show.qml" >> slideshowchanges
-  echo "    echo '            anchors.centerIn: parent' >> src/branding/custombranding/show.qml" >> slideshowchanges
-  echo "    echo '        }' >> src/branding/custombranding/show.qml" >> slideshowchanges
-  echo "    echo '    }' >> src/branding/custombranding/show.qml" >> slideshowchanges
-  currentslide=$(( $currentslide + 1 ))
-  done
-  sed -i "s/DISTRNAME/'Tedy'/g" ./customrepo/archmaker-calamares/PKGBUILD
-  sed -i "s/DISTRVERSION/${distroversion}/g" ./customrepo/archmaker-calamares/PKGBUILD
-  echo "    echo '    ' >> src/branding/custombranding/show.qml" >> slideshowchanges
-  echo "    echo '}' >> src/branding/custombranding/show.qml" >> slideshowchanges
-  lastline=$(( $currentslide + 27 ))
-  sed -i '/mkdir -p build/r slideshowchanges' ./customrepo/archmaker-calamares/PKGBUILD
-  rm slideshowchanges
-  cd ./customrepo/archmaker-calamares
-  makepkg --printsrcinfo > .SRCINFO
-  cd ../
-  echo "Building qt5-styleplugins-git..."
-  git clone https://aur.archlinux.org/qt5-styleplugins-git.git
-  cd qt5-styleplugins-git
-  makepkg -si
-  cp *.pkg.ta* ../x86_64
-  cd ../
-  echo "Building calamares..."
-  cd archmaker-calamares
-  makepkg -s
-  cp *.pkg.ta* ../x86_64
-  cd ../
-  rm -rf qt5-styleplugins-git archmaker-calamares
-  cd ../
-  echo "archmaker-calamares" | sudo tee --append ./workingdir/packages.both > /dev/null
-}
 compileaurpkgs() {
   mkdir customrepo
   mkdir customrepo/x86_64
@@ -124,12 +72,6 @@ setuprepo() {
   echo "[customrepo]" | sudo tee --append ./workingdir/pacman.conf > /dev/null
   echo "SigLevel = Never" | sudo tee --append ./workingdir/pacman.conf > /dev/null
   echo "Server = file://$(pwd)/customrepo/$(echo '$arch')" | sudo tee --append ./workingdir/pacman.conf > /dev/null
-  # sudo pacman -Syy
-  # cat /etc/pacman.conf > ./pacman.backup
-  # echo "[customrepo]" | sudo tee --append /etc/pacman.conf > /dev/null
-  # echo "SigLevel = Never" | sudo tee --append /etc/pacman.conf > /dev/null
-  # echo "Server = file://$(pwd)/customrepo/$(echo '$arch')" | sudo tee --append /etc/pacman.conf > /dev/null
-  # sudo pacman -Syy
 }
 buildtheiso() {
   sudo rm -rf ./workingdir/airootfs/etc/systemd/system/getty*
@@ -139,9 +81,6 @@ buildtheiso() {
 }
 cleanup() {
   echo "Cleaning up..."
-  # cat ./pacman.backup | sudo tee /etc/pacman.conf > /dev/null
-  # sudo pacman -Syy
-  # rm ./pacman.backup
   sudo rm -rf /var/cache/pacman/pkg/archmaker-calamares*
   sudo rm -rf /var/cache/pacman/pkg/qt5-styleplugins-git*
   finalfiles=""
@@ -160,7 +99,6 @@ createdir
 copypackages
 copyskel
 createlsbrelease
-# compilecalamares
 compileaurpkgs
 setuprepo
 buildtheiso
